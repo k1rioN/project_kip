ade6bcef3170126b7fb0fb3f10cda77d95cc4afb
Jasper Adrian <daswerwest7@gmail.com>
2020-10-20 19:33:18 +0300

    over

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,38 +1,34 @@
 <script>
-	
+	import { fade, fly } from 'svelte/transition';
+	let visible = true;
 </script>
 
 <style>
-	nav {
+	p {
 		font-weight: 300;
 		padding: 0 1em;
 		text-align: center;
 		float: inherit;
 	}
 
-	ul {
-		margin: 0;
-		padding: 0;
-		text-align: center;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+	
 
 	img {
 		width: 350px;
 		height: 200px;
 		margin-right: 10px;
-		margin-top: 90px;
+		margin-top: -10px;
+		margin-bottom: -20px;
 	}
 </style>
 
-<nav>
-	<ul>
-		<img src="gAoHnb0oeEk.jpg" alt="kip"/>
-	</ul>
-</nav>
+
+
+
+
+
+{#if visible}
+	<p in:fly="{{ y: 200, duration: 2000 }}" out:fade>
+		<img in:fly="{{ y: 200, duration: 2000 }}" out:fade src="gAoHnb0oeEk.jpg" alt="kip"/>
+	</p>
+{/if}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,10 +4,17 @@
 
 
 <script>
+	import TransitionWrapper from '../components/TransitionWrapper.svelte'
+	import Chart from 'svelte-frappe-charts';
 	let text;
-	let result_post;
-	let results;
+	let result_post = '';
+	let status = false;
+	let math = 25;
 
+
+	function switchStatus() {
+		status = true;
+	}
 
 	async function doPost () {
 		const res = await fetch(`http://d84030b6807c.ngrok.io/process`, {
@@ -23,34 +30,49 @@
 		})
 		const json = await res.json()
 		result_post = JSON.stringify(json)
-		
-		const res2 = await self.fetch(`http://d84030b6807c.ngrok.io/process`)
-			.then(async res => {				
-				results = await res.json();
-			})
-		console.log("Post done")
+		diagramStatus = true;
 	}
 
+	let data = {
+    labels: ['Математический', 'Физический', 'Экономический', 'Правовой', 'Нормативный', 'Доктринальный ', 'Организационный', 'Маркетинговый', 'Социально-компьютерный', 'Технический'],
+    datasets: [
+      {
+        values: [math, 12, 3, 9, 8, 15, 9, 10, 20, 11]
+      }
+    ]
+  };
 </script>
 
 
 <body>
-	<h1>ВВЕДИТЕ ТЕКСТ ДЛЯ АНАЛИЗА</h1>
+	<TransitionWrapper>
+		<h1>ВВЕДИТЕ ТЕКСТ ДЛЯ АНАЛИЗА</h1> <br/>
+	<p></p>
 	<textarea bind:value={text}></textarea>
-	<button on:click={doPost}>АНАЛИЗ</button>
-	<p>{results}</p>
+	<button on:click={doPost} on:click={switchStatus}>АНАЛИЗ</button>
+	<p>{result_post}</p>
+	{#if status === true}
+	{#if (process.browser) }
+	<Chart data={data} type="line" />
+	{/if}
+	{/if}
+	</TransitionWrapper>
 </body>
 
 
 
 <style>
-	textarea { width: 100%; height: 200px; }
+	textarea { width: 100%; height: 200px; margin-top: -20px; }
 	h1 {
 		text-align: center;
+		margin-top: -30px;
+		margin-bottom: 5px;
 	}
 	button {
 		position: center;
 		width: 101%;
 		height: 50px;
+		margin-top: -18px;
 	}
+	
 </style>
